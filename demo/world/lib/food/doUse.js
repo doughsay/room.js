8753b5ea833f08762df009c84c9682283bf59a26
Omikhleia <didier.willis@gmail.com>
2016-08-15 03:32:46 +0200

    [DEMO] Clean-up / Remove objects that are now useless or replace by better alternatives

--- demo/world/lib/food/doUse.js
+++
@@ -1,6 +0,0 @@
-function doUse(agent) {
-  /**
-   * Triggered when the object is eaten or drank.
-   * To be overriden by derived objects.
-   */  
-}
--- demo/world/lib/food/onUsed.js
+++
@@ -1,6 +0,0 @@
-function onUsed(agent) {
-  /**
-   * Triggered when the object is eaten or drank.
-   * To be overriden by derived objects.
-   */
-}
