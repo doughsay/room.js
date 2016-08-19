196af9c85aee35e661d873287cc6196ba93769ba
Omikhleia <didier.willis@gmail.com>
2016-08-19 19:59:28 +0200

    [DEMO] Playtest session: remove "keep" on gettable and rollback previous change on "get" - Keep: the idea was nice but too confusing for a demo - Get/Take: previous attempts at making "get" more clever, turn to be more confusing than expected (and as a side effect, permitting to get non-gettable items). The command execution logic always give precedence to the player inventory, and that's not the item faults...

--- demo/world/lib/traits/gettable/take.js
+++ demo/world/lib/traits/gettable/take.js
@@ -1,14 +1,9 @@
 function take({ player, dobj, iobj, verbstr, argstr, dobjstr, prepstr, iobjstr }) {
-  // If triggered by a item already in player inventory, check in room.
-  let target = (dobj.location === player)
-    ? player.location.findInside(dobjstr)
-    : dobj;
-  if (target === fail) {
-    player.tell(`I can't see any ${dobjstr} to take.`);
-  } else if (target === ambiguous) {
-    player.tell(`I can't tell which ${dobjstr} you meant.`);
-  } else {
-    target.location = player;
-    player.location.announce(this.announceTakeItem, player, this);
-  } 
+  if (this.location === player) {
+    player.tell("You already have that.");
+    return;
+  }
+  
+  player.location.announce(this.announceTakeItem, player, this);
+  this.location = player;
 }
