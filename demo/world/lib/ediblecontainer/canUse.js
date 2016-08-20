d80c426b422f30c3c2b868e93a99ab9bb6eba2ef
Omikhleia <didier.willis@gmail.com>
2016-08-20 12:33:14 +0200

    [DEMO] Add another interactive 'npc' with a trigger, just to show things and make the demo a bit more insteresting

--- demo/world/lib/ediblecontainer/canUse.js
+++ demo/world/lib/ediblecontainer/canUse.js
@@ -1,6 +1,6 @@
 function canUse(agent) {
   if (this.exhausted) {
-    agent.tell(`${this.containerObject} is empty.`);
+    agent.tell(`The ${this.containerObject} is empty.`);
     return false;
   }
   return true;
---
+++ demo/world/lib/traits/describable/onExamine.js
@@ -0,0 +1,6 @@
+function onExamine(player) {
+  /**
+   * Triggered when the object is examined.
+   * To be overriden by derived objects.
+   */
+}
