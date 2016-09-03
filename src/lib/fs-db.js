9842b9fbfc56434d8da178805fd8e8371d8a64b1
Omikhleia <didier.willis@gmail.com>
2016-09-03 13:36:50 +0200

    Code review on pull request

--- src/lib/fs-db.js
+++ src/lib/fs-db.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const remove = require('remove');
-const chokidar = require('chokidar');
-const EventEmitter = require('events');
-const util = require('util');
-
+const path = require('path').posix;
 // Design note:
 //   "The default operation of the path module varies based on the operating system
 //   on which a Node.js application is running. Specifically, when running on a Windows
 //   operating system, the path module will assume that Windows-style paths are being used."
 // Therefore, to avoid inconsistent behaviors, we enforce working with POSIX paths at our
 // level.
+const mkdirp = require('mkdirp');
+const remove = require('remove');
+const chokidar = require('chokidar');
+const EventEmitter = require('events');
+const util = require('util');
 
 class FsDb {
   constructor(directory, logger) {
@@ -35,7 +34,7 @@ class FsDb {
 
   load(dir = this.directory) {
     fs.readdirSync(dir).forEach(file => {
-      const filepath = path.posix.join(dir, file);
+      const filepath = path.join(dir, file);
       const stats = fs.statSync(filepath);
       if (stats.isDirectory()) {
         this.load(filepath);
@@ -56,9 +55,9 @@ class FsDb {
 
     this.watcher.on('ready', () => {
       this.watcher
-        .on('add', this.onFileAdded.bind(this))
-        .on('change', this.onFileChanged.bind(this))
-        .on('unlink', this.onFileRemoved.bind(this))
+        .on('add', file => this.onFileAdded(file.replace(/[\\]/g, '/')))
+        .on('change', file => this.onFileChanged(file.replace(/[\\]/g, '/')))
+        .on('unlink', file => this.onFileRemoved(file.replace(/[\\]/g, '/')))
         .on('error', error => {
           // On Windows notabbly, when a file is deleted from an extern process, an EPERM status
           // may occur here, due to a temporary system file lock.
@@ -74,7 +73,7 @@ class FsDb {
   }
 
   toFilepath(relpath) {
-    return path.posix.join(this.directory, relpath);
+    return path.join(this.directory, relpath);
   }
 
   createOrGetMap(map, key) {
@@ -97,7 +96,7 @@ class FsDb {
 
   mapFor(filepath, create = true) {
     const getCallback = create ? this.createOrGetMap.bind(this) : this.getMap.bind(this);
-    const keys = this.toRelpath(path.posix.dirname(filepath)).split('/').filter(x => x);
+    const keys = this.toRelpath(path.dirname(filepath)).split('/').filter(x => x);
     return keys.reduce((map, key) => getCallback(map, key), this.db);
   }
 
@@ -121,27 +120,27 @@ class FsDb {
   unloadDir(dirname) {
     const relpath = this.toRelpath(dirname);
     const map = this.mapFor(dirname);
-    const dir = path.posix.basename(dirname);
+    const dir = path.basename(dirname);
     map.delete(dir);
     this.logger.trace({ relpath }, 'unloaded directory');
-    if (this.unloadable(map)) { this.unloadDir(path.posix.dirname(dirname)); }
+    if (this.unloadable(map)) { this.unloadDir(path.dirname(dirname)); }
   }
 
   unloadFile(filepath, emit = true) {
     const relpath = this.toRelpath(filepath);
-    const file = path.posix.basename(filepath);
+    const file = path.basename(filepath);
     const originalExisted = typeof this.read(relpath) !== 'undefined';
     if (originalExisted) {
       const map = this.mapFor(filepath);
       map.delete(file);
       this.logger.trace({ relpath }, 'unloaded file');
-      if (this.unloadable(map)) { this.unloadDir(path.posix.dirname(filepath)); }
+      if (this.unloadable(map)) { this.unloadDir(path.dirname(filepath)); }
       if (emit) { this.emit('removed', relpath); }
     }
   }
 
   onFileAdded(filepath) {
-    const relpath = this.toRelpath(filepath.replace(/[\\]/g, '/'));
+    const relpath = this.toRelpath(filepath);
     this.logger.trace({ relpath }, 'added to filesystem');
     this.loadFile(filepath);
   }
@@ -149,18 +148,18 @@ class FsDb {
   onFileChanged(filepath) {
     const relpath = this.toRelpath(filepath);
     this.logger.trace({ relpath }, 'changed in filesystem');
-    this.loadFile(filepath.replace(/[\\]/g, '/'));
+    this.loadFile(filepath);
   }
 
   onFileRemoved(filepath) {
     const relpath = this.toRelpath(filepath);
     this.logger.trace({ relpath }, 'removed from filesystem');
-    this.unloadFile(filepath.replace(/[\\]/g, '/'));
+    this.unloadFile(filepath);
   }
 
   set(relpath, contents) {
     const filepath = this.toFilepath(relpath);
-    const file = path.posix.basename(filepath);
+    const file = path.basename(filepath);
     const map = this.mapFor(filepath);
     map.set(file, contents);
     return true;
@@ -168,7 +167,7 @@ class FsDb {
 
   read(relpath) {
     const filepath = this.toFilepath(relpath);
-    const file = path.posix.basename(filepath);
+    const file = path.basename(filepath);
     const map = this.mapFor(filepath, false);
     if (!map) { return void 0; }
     return map.get(file);
@@ -180,7 +179,7 @@ class FsDb {
     const originalDidntExist = typeof originalContents === 'undefined';
     this.set(relpath, contents);
     if (originalDidntExist || contents !== originalContents) {
-      if (originalDidntExist) { mkdirp.sync(path.posix.dirname(filepath)); }
+      if (originalDidntExist) { mkdirp.sync(path.dirname(filepath)); }
       fs.writeFileSync(filepath, contents);
       this.logger.trace({ relpath }, 'wrote file to disk');
     }
@@ -236,6 +235,11 @@ class FsDb {
 
   lsFiles(relpath) {
     return this.lsBy(relpath, x => x.file);
+  }
+
+  hasContents(relpath, ext) {
+    const entries = this.lsBy(relpath, x => x.file && x.file.endsWith(ext));
+    return entries.length !== 0;
   }
 
   _inspectTree(map = this.db, prefix = '', lines = []) {
